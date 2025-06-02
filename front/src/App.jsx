fa03ad334daa3b2d56e1f1b02a26b360486ce2ee
ataraxia7899 <ataraxia7899@gmail.com>
2025-06-02 14:41:52 +0900

    PlanRecommend 컴포넌트를 App.jsx에 추가하고 주석 처리된 경로를 정리함

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,6 +7,10 @@ import Footer from "./components/Footer/Footer";
 import Ester from "./components/Ester/Ester";
 import TossPaymentTest from "./components/TossPaymentTest/TossPaymentTest";
 import Cart from "./components/Cart/Cart";
+// import Phone from "./components/Phone/Phone";
+// import Detail from "./components/Detail/Detail";
+// import Mypage from "./components/Mypage/Mypage";
+import PlanRecommend from "./components/PlanRecommend/PlanRecommend";
 
 function App() {
     return (
@@ -15,10 +19,17 @@ function App() {
                 <Nav />
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
+                    {/* <Route path="/Phone" element={<Phone />}></Route>
+                    <Route path="/Detail" element={<Detail />}></Route> */}
                     <Route path="/LogIn" element={<LogIn />}></Route>
                     <Route path="/SignUp" element={<SignUp />}></Route>
+                    {/* <Route path="/Mypage" element={<Mypage />}></Route> */}
                     <Route path="/Ester" element={<Ester />}></Route>
                     <Route path="/Cart" element={<Cart />}></Route>
+                    <Route
+                        path="/api/smartchoice/recommend"
+                        element={<PlanRecommend />}
+                    ></Route>
                 </Routes>
                 <Footer />
             </BrowserRouter>
